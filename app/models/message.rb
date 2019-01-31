be0361d8462159586d93db1635948b09abbc4d6f
Дмитрий <safonovdmitr@gmail.com>
2019-01-31 16:35:30 +0300

    add gem annotate

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id                  :integer          not null, primary key
+#  messanger           :string           not null
+#  body                :text             not null
+#  recipient           :string           not null
+#  time_of_the_sending :datetime         not null
+#  status              :string           not null
+#  number              :integer          not null
+#  number_max          :integer          not null
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+
 class Message < ApplicationRecord
 end
