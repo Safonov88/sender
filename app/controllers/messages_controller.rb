3a194138961b7aa9c2b73f546826f5535773ca17
Дмитрий <safonovdmitr@gmail.com>
2019-01-31 20:40:04 +0300

    create messages/index

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,12 @@
 class MessagesController < ApplicationController
+  def index; end
+
   def create
+
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:sender_id, :messanger, :body, :recipient, :time_of_the_sending)
   end
 end
