35bedfb2689a2d7d8a8bfb2a81846f6c1a009d48
Дмитрий <safonovdmitr@gmail.com>
2019-02-02 21:06:14 +0300

    create MessageService

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,18 +2,7 @@ class MessagesController < ApplicationController
   def index; end
 
   def create
-    prm = message_params
-
-    prm[:time_of_the_sending] = Time.now if prm[:time_of_the_sending].blank? || (prm[:time_of_the_sending] < Time.current)
-
-    prm[:messanger].each do |m|
-      prm[:recipient].each do |r|
-        message = Message.new(sender_id: prm[:sender_id], messanger: m, body: prm[:body],
-                              recipient: r, time_of_the_sending: prm[:time_of_the_sending].to_datetime - 3.hours, number_max: Message::NUMBER_MAX)
-
-        SendMessageJob.set(wait_until: message.time_of_the_sending).perform_later message if message.save
-      end
-    end
+    MessageService.call(message_params)
   end
 
   private
---
+++ app/services/message_service.rb
@@ -0,0 +1,29 @@
+class MessageService
+  extend self
+
+  def call(prm)
+    @prm = prm
+    @message = ''
+    current_time_of_the_sending
+    separate_messages
+  end
+
+  def current_time_of_the_sending
+    @prm[:time_of_the_sending] = Time.now if @prm[:time_of_the_sending].blank? || (@prm[:time_of_the_sending] < Time.current)
+  end
+
+  def separate_messages
+    @prm[:messanger].each do |m|
+      @prm[:recipient].each do |r|
+        @message = Message.new(sender_id: @prm[:sender_id], messanger: m, body: @prm[:body],
+                              recipient: r, time_of_the_sending: @prm[:time_of_the_sending].to_datetime - 3.hours, number_max: Message::NUMBER_MAX)
+
+        create_job
+      end
+    end
+  end
+
+  def create_job
+    SendMessageJob.set(wait_until: @message.time_of_the_sending).perform_later @message if @message.save
+  end
+end
