2ca804e6b22e7d78187b1b2f24aab155e3288d20
Дмитрий <safonovdmitr@gmail.com>
2019-02-03 17:24:45 +0300

    config test db and fix factory message

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :message do
-    sender_id 1
-    messanger %i(telegram viber whatsapp)
-    body 'Тестовое сообщение'
-    recipient ["user #{rand(1..10)}", "user #{rand(11..20)}"]
-    time_of_the_sending Time.now
+    sender_id { 1 }
+    messanger { :telegram }
+    body { 'Тестовое сообщение' }
+    recipient { ["user #{rand(1..10)}", "user #{rand(11..20)}"] }
+    time_of_the_sending { Time.now }
+    number_max { Message::NUMBER_MAX }
   end
 end
